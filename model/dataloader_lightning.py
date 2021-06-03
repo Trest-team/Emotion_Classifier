fe1f9fdbde5e93b7fc5384528bd1494832b0a309
Jeoung-Minju <jminju254@gmail.com>
2021-06-03 15:15:04 +0900

    dataloader by pytorch_lightning

---
+++ model/dataloader_lightning.py
@@ -0,0 +1,94 @@
+import pytorch_lightning as pl
+import pandas as pd
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
+from torch import functional as F
+from transformers import BartTokenizer
+
+
+class ChatDataSet(Dataset):
+    def __init__(self, file_path, vocab_path, merge_file, max_seq_len = 128, bos_token = '<bos>', eos_token = '<eos>'):
+        super().__init__()
+
+        self.dataset = pd.read_csv(file_path)
+        self.file_path = file_path
+        self.vocab_path = vocab_path
+        self.merges_file = merge_file
+        self.max_seq_len = max_seq_len
+        self.bos_token = bos_token
+        self.eos_token = eos_token
+
+        self.tokenizer = BartTokenizer(
+            vocab_file = self.vocab_path,
+            merges_file = self.merges_file,
+            bos_token = self.bos_token,
+            eos_token = self.eos_token,
+        )
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def make_input_id_mask(self, tokens, index):
+        input_id = self.tokenizer.convert_tokens_to_ids(tokens)
+        mask = [1] * len(input_id)
+
+        if len(input_id) < self.max_seq_len:
+            while len(input_id) < self.max_seq_len:
+                input_id += [self.tokenizer.pad_token_id]
+                mask += [0]
+
+        else:
+            input_id = input_id[: self.max_seq_len - 1] + [self.tokenizer.eos_token_id]
+            mask = mask[: self.max_seq_len]
+        return input_id, mask
+
+    def __getitem__(self, index):
+        record = self.dataset.iloc[index]
+        q, l = record['Q'], record['label']
+
+        q_tokens = [self.bos_token] + self.tokenizer.tokenize(q) + [self.eos_token]
+        l_tokens = self.tokenizer.tokenize(l)
+
+        input_id, masking = self.make_input_id_mask(q_tokens, index)
+        label = self.tokenizer.convert_tokens_to_ids((l_tokens))
+
+        return {
+            'input_ids' : np.array(input_id, dtype = np.int_),
+            'mask' : np.array(masking, dtype = np.float_),
+            'labels' : np.array(label, dtype = np.int_)
+        }
+
+class DataModule(pl.LightningDataModule):
+    def __init__(self, train_file, val_file, test_file, vocab_path, merge_file, batch_size = 64, max_seq_len = 128, num_workers = 5):
+        super().__init__()
+        self.batch_size = batch_size
+        self.max_seq_len = max_seq_len
+
+        self.train_file_path = train_file
+        self.val_file_path = val_file
+        self.test_file_path = test_file
+
+        self.vocab_path = vocab_path
+        self.merges_file = merge_file
+
+        self.num_workers = num_workers
+
+    def prepare_data(self):
+        pass
+
+    def setup(self, stage = None):
+        self.emotion_train = ChatDataSet(self.train_file_path, self.vocab_path, self.merges_file)
+        self.emotion_val = ChatDataSet(self.val_file_path, self.vocab_path, self.merges_file)
+        self.emotion_test = ChatDataSet(self.test_file_path, self.vocab_path, self.merges_file)
+    
+    def train_dataloader(self):
+        emotion_train = DataLoader(self.emotion_train, batch_size = self.batch_size)
+        return emotion_train
+    
+    def val_dataloader(self):
+        emotion_val = DataLoader(self.emotion_val, batch_size = self.batch_size)
+        return emotion_val
+
+    def test_dataloader(self):
+        emotion_test = DataLoader(self.emotion_test, batch_size = self.batch_size)
+        return emotion_test
